58ec9a0c73cb54e98f15a51ab1e608f1d6618d91
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-21 21:46:19 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,28 +3,45 @@ import os
 from flask import Flask, request, jsonify
 from datetime import datetime, date, time, timezone
 from elasticsearch import Elasticsearch
+from marshmallow import Schema, fields, ValidationError
 import json
 
-elk_host = os.environ['elk_host']
+   
+class BaseSchema(Schema):
+    log_type = fields.String(required=True)
+    message = fields.String(required=True)
+    version = fields.Integer(required=True)
+
+# set ElasticSearch host
+client = Elasticsearch(hosts=[os.environ['elk_host']])
 
 app = Flask('flaskapp')
 
 # set the proper GET endpoint
 @app.route('/logs', methods=["POST"])
 def index():
-    product = request.args.get('product')
-    json_element = {'product': product}
-    
-    # set ElasticSearch host
-    client = Elasticsearch(hosts=["http://elk_host:80"])
-    
-    # send the json to elastic using Elastic python module
+     # Get Request body from JSON
+    request_data = request.json
+    schema = BaseSchema()
     try:
-        response = client.index(index='api-index', doc_type='_doc', body=json_element)
-    except Exception as e:
-        return f'Error sending index to elastic search. {e}'
+        # Validate request body against schema data types
+        result = schema.load(request_data)
+    except ValidationError as err:
+        # Return a nice message if validation fails
+        result = False
+        return jsonify(err.messages), 400
     
-    return json_element
+    if result:
+        product = request.args.get('product')
+        json_element = {'product': product}
+    
+        # send the json to elastic using Elastic python module
+        try:
+            response = client.index(index='api-index', doc_type='_doc', body=json_element)
+        except Exception as e:
+            return f'Error sending index to elastic search. {e}'
+
+        return json_element
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
