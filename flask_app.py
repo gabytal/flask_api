f27996c8bac0e0657194a49d4c7c7a7e695c537e
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-22 00:24:47 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,8 @@ import logging
 logs_format = '[%(asctime)s] %(levelname)s - %(message)s'
 logger = logging.getLogger()
 
+es_host = os.environ['elk_host']
+
 # this is needed in order to see the logs through K8s logs
 # '/proc/1/fd/1' file is functioning as the Pod's STDOUT.
 handler = logging.FileHandler('/proc/1/fd/1', mode='w')
@@ -25,22 +27,21 @@ class BaseSchema(Schema):
 
 
 # set ElasticSearch host
-logger.debug(f"Setting ES host: {os.environ['elk_host']}")
-client = Elasticsearch(hosts=[f"{os.environ['elk_host']}:80"])
+logger.debug(f"Setting ES host: {es_host}")
+client = Elasticsearch(hosts=[f"{es_host}:80"])
 
 app = Flask('flaskapp')
 
 
 # set the proper GET endpoint
-@ app.route('/logs', methods=["POST"])
+@app.route('/logs', methods=["POST"])
 def index():
-    
     # Get Request body from JSON
     request_data = request.json
-    
+
     # set the input schema
     schema = BaseSchema()
-    
+
     result = False
     try:
         # Validate request body against schema data types
@@ -64,6 +65,8 @@ def index():
         except Exception as e:
             logger.error("Could not send data to ES!")
             return f'Error sending index to elastic search. {e}'
+        if response:
+            logger.info("Data successfully sent to ES!")
         return json_element
 
 
