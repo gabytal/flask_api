e9d06cd12d97a6f58ed06e335ba09e8b8d807cbc
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-22 00:31:37 +0300

    Update test_api_functionality.sh

--- test_api_functionality.sh
+++ test_api_functionality.sh
@@ -2,12 +2,12 @@
 # this script purpose is to test the API, by sending get request and expect 200 Status Code.
 
 # sending a POST request and exporting the status code
-status_code=$(curl -k --silent --output /dev/null --write-out "%{http_code}" https://localhost/logs?12124)
+status_code=$(curl -H "Content-Type: application/json" -X POST  -d '{"log_type":"CI_TEST", "message": "CI_TEST", "version": 1}' --write-out "%{http_code}"  http://localhost:5000/logs)
 
 if test "$status_code" -ne 200; then
     echo "Response status code: $status_code"
     echo "there is a problem with API, please check!"
-    exit 2
+    exit 1
 else
     echo "Response status code: $status_code"
     echo "The API is OK!"
