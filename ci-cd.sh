1fbe8fc742a6f0d8a207a70030e21955e081dc3d
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-21 23:30:44 +0300

    Update ci-cd.sh

--- ci-cd.sh
+++ ci-cd.sh
@@ -7,6 +7,8 @@ cd /home/ubuntu/flask_api/ &&
 docker build -t flask-app:$version . &&
 docker run -d --name flask-app -p 5000:5000 --env elk_host="$elk_host" flask-app:"$version" &&
 
+chmod 555 test_api_functionality.sh
+
 ./test_api_functionallity.sh
 if [ $? -eq 0 ]; then
     echo "the API is is fucntioning!"
@@ -15,16 +17,17 @@ else
     exit 1
 fi
 
-# clean env
-cd .. && rm -Rf flask_api/
-docker rm -f flask-app
-echo "CI Process has been passed."
 
+echo "CI Process has been passed."
 echo "Pushing Atrifact - flask-app:"$version" to ECR."
 
 docker login "$ecr_repo"
 docker push flask-app:"$version"
 docker image prune -a --force
 
+# clean env
+echo "Cleaning Testing Enviornment."
+cd .. && rm -Rf flask_api/
+docker rm -f flask-app
 
 
