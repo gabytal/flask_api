90c03164451b874d8c88242a36954b76b32a881d
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-22 00:13:34 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,3 @@
-        
 # import required modules
 import os
 from flask import Flask, request, jsonify
@@ -18,6 +17,7 @@ formatter = logging.Formatter(logs_format)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 class BaseSchema(Schema):
     log_type = fields.String(required=True)
     message = fields.String(required=True)
@@ -25,8 +25,8 @@ class BaseSchema(Schema):
 
 
 # set ElasticSearch host
-logger.debug(f"Setting ES host: {os.environ["elk_host"]}")
-client = Elasticsearch(hosts=[f'{os.environ["elk_host"]}:80'])
+logger.debug(f"Setting ES host: {os.environ['elk_host']}")
+client = Elasticsearch(hosts=[f"{os.environ['elk_host']}:80"])
 
 app = Flask('flaskapp')
 
@@ -34,10 +34,13 @@ app = Flask('flaskapp')
 # set the proper GET endpoint
 @ app.route('/logs', methods=["POST"])
 def index():
+    
     # Get Request body from JSON
     request_data = request.json
+    
+    # set the input schema
     schema = BaseSchema()
-
+    
     result = False
     try:
         # Validate request body against schema data types
@@ -47,13 +50,13 @@ def index():
         # Return a nice message if validation fails
         logger.error("Request body validations has faild!")
         return jsonify(err.messages), 400
-    
+
     if result:
         logger.info("Request body validations has passed!")
-        logger.debug("Praparing data for ES")
+        logger.debug("Preparing data for ES")
         json_element = {'log_type': request_data['log_type'],
-                        'message': request_data['message']
-                       'version': request_data['version']}
+                        'message': request_data['message'],
+                        'version': request_data['version']}
         # send the json to elastic using Elastic python module
         try:
             logger.debug(f"Sending data for ES: {json_element}")
@@ -62,6 +65,7 @@ def index():
             logger.error("Could not send data to ES!")
             return f'Error sending index to elastic search. {e}'
         return json_element
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
