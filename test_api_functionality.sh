71286511a233834f56d11a31e8a849b5851dfd6b
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-21 21:50:42 +0300

    Add files via upload

---
+++ test_api_functionality.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# this script purpose is to test the API, by sending get request and expect 200 Status Code.
+
+# sending a /system_load request and exporting the status code
+status_code=$(curl -k --silent --output /dev/null --write-out "%{http_code}" https://localhost)
+
+if test "$status_code" -ne 200; then
+    echo "Response status code: $status_code"
+    echo "there is a problem with API, please check!"
+    exit 2
+else
+    echo "Response status code: $status_code"
+    echo “The API is OK!"
+fi
