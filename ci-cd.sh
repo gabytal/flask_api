40b40404140d7c2fe8a2c3f9464115c1311bddc2
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-22 04:44:31 +0300

    Update ci-cd.sh

--- ci-cd.sh
+++ ci-cd.sh
@@ -29,7 +29,7 @@ echo
 echo "running Application container for testing" &&
 echo
 
-rm -f flask-app-test
+rm -f flask-app-test && 
 docker run -d --name flask-app-test -p 4500:5000 --env elk_host="$elk_host" flask-app:"$version" &&
 if [ $? -eq 0 ]; then
     echo
