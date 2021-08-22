f72c2015d05199fc20a42ea93f4e023822c18d4b
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-08-22 04:39:20 +0300

    Update test_api_functionality.sh

--- test_api_functionality.sh
+++ test_api_functionality.sh
@@ -2,7 +2,7 @@
 # this script purpose is to test the API, by sending get request and expect 200 Status Code.
 
 # sending a POST request and exporting the status code
-status_code=$(curl -H "Content-Type: application/json" -X POST  -d '{"log_type":"CI_TEST", "message": "CI_TEST", "version": 1}' --write-out "%{http_code}"  http://localhost:5000/logs)
+status_code=$(curl -H "Content-Type: application/json" -X POST  -d '{"log_type":"CI_TEST", "message": "CI_TEST", "version": 1}' --write-out "%{http_code}"  http://localhost:4500/logs)
 
 if test "$status_code" -ne 200; then
     echo "Response status code: $status_code"
